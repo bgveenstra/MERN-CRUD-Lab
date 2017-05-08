faf8e99790a998e8df942fd9f4dd5c5b6bd52021
bgveenstra <b.g.veenstra@gmail.com>
2017-05-08 14:32:00 -0700

    add server routes for comments, change port

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     Comment = require('./model/comments');
 
+
+mongoose.connect('mongodb://localhost/mern-comment-app');
+
 //create instances
 var app = express(),
     router = express.Router();
 
-// set port to env or 3000
-var port = process.env.PORT || 3001;
+// set port to env or 4000
+var port = process.env.PORT || 4000;
 
 
 //config API to use bodyParser and look for JSON in req.body
@@ -30,12 +33,83 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.get('/', function(req,res){
+  res.send('server running');
+})
+
 //set route path and init API
 router.get('/', function(req,res) {
   res.json({message: 'API Initialized!'});
 });
 
+// delete all comments
+router.route('/nuke').get(function(req,res){
+  Comment.remove(function(err,succ){
+  res.json(succ);
+  });
+});
+
+//adding the /comments route to our /api router
+router.route('/comments')
+  //retrieve all comments from the database
+  .get(function(req, res) {
+    //looks at our Comment Schema
+    Comment.find(function(err, comments) {
+      if (err)
+        res.send(err);
+      //responds with a json object of our database comments.
+      res.json(comments)
+    });
+  })
+  //post new comment to the database
+  .post(function(req, res) {
+    var comment = new Comment();
+    //body parser lets us use the req.body
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+
+    comment.save(function(err) {
+      if (err){
+        res.send(err);
+      } else {
+        res.json(comment);
+      }
+    });
+  });
+
+  router.route('/comments/:comment_id')
+  //The put method gives us the chance to update our comment based on the ID passed to the route
+   .put(function(req, res) {
+     Comment.findById(req.params.comment_id, function(err, comment) {
+       if (err)
+         res.send(err);
+       //setting the new author and text to whatever was changed. If nothing was changed
+       // we will not alter the field.
+       (req.body.author) ? comment.author = req.body.author : null;
+       (req.body.text) ? comment.text = req.body.text : null;
+       //save comment
+       comment.save(function(err) {
+         if (err)
+           res.send(err);
+         res.json({ message: 'Comment has been updated' });
+       });
+     });
+   })
+   //delete method for removing a comment from our database
+   .delete(function(req, res) {
+     //selects the comment by its ID, then removes it.
+     Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+       if (err)
+         res.send(err);
+       res.json({ message: 'Comment has been deleted' })
+     })
+   });
+
+//use router config when we call /API
+app.use('/api', router);
+
+
 //start server
-app.listen(process.env.PORT, function() {
+app.listen(port, function() {
   console.log(`api running on port ${port}`);
 });
