a625b648439a7cc1e30840d934b66c870bd0b0e8
bgveenstra <b.g.veenstra@gmail.com>
2017-05-08 14:32:28 -0700

    update api url in src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 
+
+// const apiUrl = 'https://mern-comment-box-api.herokuapp.com/comments';
+const port = process.env.PORT || 4000;
+const apiUrl = `http://localhost:${port}/api/comments`;
+
 ReactDOM.render(
   <CommentBox
-    url='https://mern-comment-box-api.herokuapp.com/comments' pollInterval={2000} />,
+    url={apiUrl} pollInterval={2000} />,
   document.getElementById('root')
 );
